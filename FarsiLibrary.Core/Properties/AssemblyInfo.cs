6e592a08772692abd0b615a6eaf11d85fe714815
Ingo <thewatcher@watchersnet.de>
2023-07-16 11:30:24 +0200

    rename library to FarsiLibrary.Core

---
+++ FarsiLibrary.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: CLSCompliant(true)]
+
+[assembly: InternalsVisibleTo("FarsiLibrary.Tests, PublicKey=00240000048000009400000006020000002400005253413100040000010001000bd06c5f2139bb49786f0f662880bd6e984a64b40bfbedbd39f5893396c22b88366c9e1586bd2c9758e28063266d1b4cd521b89b9eb5f6332b4bf780fbb94926bd31416c0f4f2c4c4d95bc300241602447fdbf2b17cda756c51c4831f6f314fab2296bf319d9dbd2ef3a05dda68fd8495642aa5d376bfdd6fbfce193448893c4")]
+
+[assembly: AssemblyTrademark("Farsi Library and Controls by Hadi Eskandari (H.Eskandari@Gmail.com). All rights are reserved for the author. Contact the author to obtain license to use this product.")]
---
+++ FarsiLibrary.Core/Utils/Formatter/BasicTimeFormat.cs
@@ -0,0 +1,136 @@
+using System;
+using FarsiLibrary.Utils.Internals;
+
+namespace FarsiLibrary.Utils.Formatter;
+
+using System.Globalization;
+
+public class BasicTimeFormat : ITimeFormat
+{
+    public static string Negative = "-";
+    public static string Sign = "%s";
+    public static string Quantity = "%n";
+    public static string Unit = "%u";
+
+    public double RoundingTolerance { get;set; }
+    public string Pattern { get;set; }
+    public string FuturePrefix { get; set; }
+    public string FutureSuffix { get;set; }
+    public string PastPrefix { get; set; }
+    public string PastSuffix { get; set; }
+
+    public BasicTimeFormat()
+    {
+        this.RoundingTolerance = 0;
+        this.Pattern = string.Empty;
+        this.FuturePrefix = string.Empty;
+        this.FutureSuffix = string.Empty;
+        this.PastPrefix = string.Empty;
+        this.PastSuffix = string.Empty;
+    }
+
+    public string Format(Duration duration)
+    {
+        var sign = GetSign(duration);
+        var quantity = this.GetQuantity(duration);
+        var unit = GetGramaticallyCorrectName(duration, quantity);
+        var result = this.ApplyPattern(sign, unit, quantity);
+
+        result = this.Decorate(sign, result);
+
+        return result;
+    }
+
+    private string Decorate(string sign, string result)
+    {
+        if (sign == Negative)
+        {
+            result = this.PastPrefix + " " + result + " " + this.PastSuffix;
+        }
+        else
+        {
+            result = this.FuturePrefix + " " + result + " " + this.FutureSuffix;
+        }
+
+        return result.Trim();
+    }
+
+    private string ApplyPattern(string sign, string unit, double quantity)
+    {
+        var result = this.Pattern.Replace(Sign, sign);
+        var number = FormatNumber(quantity);
+
+        result = result.Replace(Quantity, number);
+        result = result.Replace(Unit, unit);
+
+        return result;
+    }
+
+    private static string FormatNumber(double quantity)
+    {
+        return CultureHelper.IsFarsiCulture() ? ToWords.ToString(quantity) : quantity.ToString(CultureInfo.InvariantCulture);
+    }
+
+    private double GetQuantity(Duration duration)
+    {
+        var quantity = Math.Abs(duration.Quantity);
+
+        if (duration.Delta != 0)
+        {
+            var threshold = Math.Abs(duration.Delta / duration.Unit.MillisPerUnit * 100);
+            if (threshold < this.RoundingTolerance)
+            {
+                quantity += 1;
+            }
+        }
+
+        return Math.Truncate(quantity);
+    }
+
+    private static string GetGramaticallyCorrectName(Duration d, double quantity)
+    {
+        var result = d.Unit.Name;
+        var value = Math.Abs(quantity);
+        if (value is 0 or > 1)
+        {
+            result = d.Unit.PluralName;
+        }
+
+        return result;
+    }
+
+    private static string GetSign(Duration d)
+    {
+        return d.Quantity < 0 ? Negative : string.Empty;
+    }
+
+    public BasicTimeFormat SetPattern(string pattern)
+    {
+        this.Pattern = pattern;
+        return this;
+    }
+
+    public BasicTimeFormat SetFuturePrefix(string futurePrefix)
+    {
+        this.FuturePrefix = futurePrefix.Trim();
+        return this;
+    }
+
+    public BasicTimeFormat SetFutureSuffix(string futureSuffix)
+    {
+        this.FutureSuffix = futureSuffix.Trim();
+        return this;
+    }
+
+    public BasicTimeFormat SetPastPrefix(string pastPrefix)
+    {
+        this.PastPrefix = pastPrefix.Trim();
+        return this;
+    }
+
+    public BasicTimeFormat SetPastSuffix(string pastSuffix)
+    {
+        this.PastSuffix = pastSuffix.Trim();
+        return this;
+    }
+}
